11485117587c34c258a12414227002323cdd01b9
Blackened Justice <yrochka109@mail.ru>
2016-12-14 23:41:30 +0300

    Added figures and lots of bugs. We need to fix 'em all

--- src/View/ScreenDisplay.java
+++ src/View/ScreenDisplay.java
@@ -8,17 +8,22 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import Piece.*;
+
 /**
  * Created by yury on 13.12.16.
  */
 public class ScreenDisplay extends JFrame implements View {
-    private int width = 400, height = 400 + 30;
-    public ChessPanel boardPane;
-    public JPanel settingsPane;
+    private int width = 545, height = 545 + 26;
+    private ChessPanel boardPane;
+    private JPanel settingsPane;
+
+    private Board chessboard = Board.INSTANCE;
 
     // Singleton
     public static ScreenDisplay INSTANCE = new ScreenDisplay();
     private ScreenDisplay() {
+        super("Chess");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel contentPane = new JPanel();
 
@@ -46,6 +51,21 @@ public class ScreenDisplay extends JFrame implements View {
 
     public void update() {
         boardPane.repaint();
+        // Activate this to monitor the state of every board piece
+        for (int i = 0; i < chessboard.pieces.length; ++i) {
+            Piece p = chessboard.pieces[i];
+            System.out.println(p.getX() + " " + p.getY() + " " + p.getType() + " " + p.isAlive());
+        }
+        System.out.println(chessboard.getTurn());
+
+        for (int j = 7; j >= 0; --j) {
+            for (int i = 0; i < 8; ++i) {
+                Piece p = chessboard.getPiece(i, j).get();
+                if (p != null) {
+                    boardPane.drawFigure(p.getType(), p.getColour(), i, j);
+                }
+            }
+        }
     }
 
     public void checkHandler() {
@@ -83,16 +103,69 @@ public class ScreenDisplay extends JFrame implements View {
 }
 
 class ChessPanel extends JPanel {
-    private BufferedImage[] images = new BufferedImage[13];
+    private BufferedImage currentBoard;
+    private BufferedImage boardImg;
+    private BufferedImage[] figuresImg = new BufferedImage[12];
 
     public ChessPanel() {
         try {
-            images[0] = ImageIO.read(new File("images/board.jpg"));
+            currentBoard = ImageIO.read(new File("images/board.jpg"));
         } catch (IOException e) {}
+
+        loadImages();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(images[0], 0, 0, null);
+        g.drawImage(currentBoard, 0, 0, null);
+    }
+
+    public void drawFigure(char type, boolean colour, int x, int y) {
+        Graphics g = getGraphics();
+        int startX = 27 + 60 * x + x, startY = 27 + 60 * y + y;
+
+        switch (type) {
+            case 'K':
+                if (colour) g.drawImage(figuresImg[10], startX, startY, null);
+                else g.drawImage(figuresImg[11], startX, startY, null);
+                break;
+            case 'N':
+                if (colour) g.drawImage(figuresImg[4], startX, startY, null);
+                else g.drawImage(figuresImg[5], startX, startY, null);
+                break;
+            case 'B':
+                if (colour) g.drawImage(figuresImg[6], startX, startY, null);
+                else g.drawImage(figuresImg[7], startX, startY, null);
+                break;
+            case 'p':
+                if (colour) g.drawImage(figuresImg[0], startX, startY, null);
+                else g.drawImage(figuresImg[1], startX, startY, null);
+                break;
+            case 'Q':
+                if (colour) g.drawImage(figuresImg[8], startX, startY, null);
+                else g.drawImage(figuresImg[9], startX, startY, null);
+                break;
+            case 'R':
+                if (colour) g.drawImage(figuresImg[2], startX, startY, null);
+                else g.drawImage(figuresImg[3], startX, startY, null);
+                break;
+        }
+    }
+
+    private void loadImages() {
+        try {
+            figuresImg[0] = ImageIO.read(new File("images/pawn-b.png"));
+            figuresImg[1] = ImageIO.read(new File("images/pawn-w.png"));
+            figuresImg[2] = ImageIO.read(new File("images/rook-b.png"));
+            figuresImg[3] = ImageIO.read(new File("images/rook-w.png"));
+            figuresImg[4] = ImageIO.read(new File("images/knight-b.png"));
+            figuresImg[5] = ImageIO.read(new File("images/knight-w.png"));
+            figuresImg[6] = ImageIO.read(new File("images/bishop-b.png"));
+            figuresImg[7] = ImageIO.read(new File("images/bishop-w.png"));
+            figuresImg[8] = ImageIO.read(new File("images/queen-b.png"));
+            figuresImg[9] = ImageIO.read(new File("images/queen-w.png"));
+            figuresImg[10] = ImageIO.read(new File("images/king-b.png"));
+            figuresImg[11] = ImageIO.read(new File("images/king-w.png"));
+        } catch (IOException e) {}
     }
 }
